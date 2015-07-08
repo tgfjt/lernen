e248e28cfbbce348756b63f052720026f2320870
tgfjt <tgfjt.mail@gmail.com>
2015-07-08 16:39:53 +0900

    list

---
+++ 2015/July/08.hs
@@ -0,0 +1,55 @@
+doubleMe x = x + x
+
+doubleUs x y = doubleMe x + doubleMe y
+
+wSmallNumber x = if x > 100
+                  then x
+                  else x * 2
+
+wSmallNumber' x = (if x > 100
+                    then x
+                    else x * 2) + 1
+
+joinListInt = 0:([1,2,3,4] ++ [5,6,7,8])
+-- [0,1,2,3,4,5,6,7,8]
+
+joinListStr = 'a':(" ice" ++ " candy")
+-- "a ice candy"
+
+pick3ofList = ([6,6,6] : [[1,2,3], [5,3,3], [1,2,2], [9,8]] ++ [[1,1]]) !! 3
+--[1,2,2]
+
+samplist = [5,4,3]
+
+headsample = head samplist
+--5
+
+tailsample = tail samplist
+--[4,3]
+
+lastsample = last samplist
+--3
+
+initsample = init samplist
+--[5,4]
+
+lengthsample = length samplist
+--3
+
+nullsample = null samplist
+--False
+
+reversesample = reverse samplist
+--[3,4,5]
+
+takesample = take 2 samplist
+--[5,4]
+
+elemsample = elem 4 samplist
+--True
+
+--減少列はこれじゃダメで空リストになる [10..1] = []
+--なので、こうする。[10,9..1]
+--遅延評価なのでデカイリストは作らなくて済む
+rangesample = take 5 [1,3..33 * 100]
+--[1,3,5,7,9]
--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-[haskell-tutorial](https://github.com/gree/haskell-tutorial)
+# Haskell! Haskell!
 
 
 `brew install ghc`
+
+
+## ghci
+
+* load module `:l` = `:load`
+* reload module `:r` = `:reload`
+* quit ghci `:q` = `:quit`
+
+
+## Chrome
+
+* open `chrome://settings/searchEngines`
+* haskell: `https://wiki.haskell.org/index.php?search=%s`
+
+
+## Learning
+
+* [haskell-tutorial](https://github.com/gree/haskell-tutorial)
+* [learnyouhaskell](http://learnyouahaskell.com/)
+	* すごいHaskell
